a2d382c19959328aebfca4f3167f4369123f7af8
Jash Doshi <shirothegamer69@gmail.com>
2023-04-16 02:44:17 +0530

    sadas

--- frontend-website/src/pages/Landing.jsx
+++ frontend-website/src/pages/Landing.jsx
@@ -1,47 +1,46 @@
-import React,{useRef} from 'react'
-import {useNavigate} from 'react-router-dom'
-import Navbar from '../components/Landing/Navbar/Navbar'
-import './Landing.css'
-import PopularPrograms from '../components/Landing/PopularPrograms/PopularPrograms'
-import useWindowDimensions from '../components/useWindowDimensions'
-import { dataDigitalBestSeller} from '../Data/Data'
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Landing/Navbar/Navbar";
+import "./Landing.css";
+import PopularPrograms from "../components/Landing/PopularPrograms/PopularPrograms";
+import useWindowDimensions from "../components/useWindowDimensions";
+import { dataDigitalBestSeller } from "../Data/Data";
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-import bgImage3 from '../imgs/bgImage.png'
-import bgImage2 from '../imgs/bgImage2.png'
-import approveBadge from '../imgs/ApproveBadge.svg'
-import therapyBadge from '../imgs/physical-therapy.svg'
-import trophy from '../imgs/Trophy.svg'
-import fire from '../imgs/fire.svg'
-import promo from '../imgs/Promo.svg'
-import TestimonialIcon from '../imgs/TestimonialIcon.svg'
-import TestimonialCard from '../components/Landing/Testimonials/TestimonialCard'
-import ServiceCard from '../components/ServiceCard'
-import PricingCard from '../components/PricingCard'
-import emailImg from '../imgs/email.png'
-import phoneImg from '../imgs/phone-call.png'
-import heart from '../imgs/Heart.png'
-import Slider from 'react-slick'
+import bgImage3 from "../imgs/bgImage.png";
+import bgImage2 from "../imgs/bgImage2.png";
+import approveBadge from "../imgs/ApproveBadge.svg";
+import therapyBadge from "../imgs/physical-therapy.svg";
+import trophy from "../imgs/Trophy.svg";
+import fire from "../imgs/fire.svg";
+import promo from "../imgs/Promo.svg";
+import TestimonialIcon from "../imgs/TestimonialIcon.svg";
+import TestimonialCard from "../components/Landing/Testimonials/TestimonialCard";
+import ServiceCard from "../components/ServiceCard";
+import PricingCard from "../components/PricingCard";
+import emailImg from "../imgs/email.png";
+import phoneImg from "../imgs/phone-call.png";
+import heart from "../imgs/Heart.png";
+import Slider from "react-slick";
 
-import about1 from '../imgs/about/1.jpg'
-import about2 from '../imgs/about/about1.jpeg'
-import about3 from '../imgs/about/about4.jpeg'
-import about4 from '../imgs/about/more2.jpg'
-import about5 from '../imgs/about/5.jpg'
+import about1 from "../imgs/about/1.jpg";
+import about2 from "../imgs/about/about1.jpeg";
+import about3 from "../imgs/about/about4.jpeg";
+import about4 from "../imgs/about/more2.jpg";
+import about5 from "../imgs/about/5.jpg";
 
 function Landing() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-
-  const aboutusRef = useRef(null)
-  const contactusRef = useRef(null)
-  function aboutusScroll(){
-    aboutusRef.current.scrollIntoView()
+  const aboutusRef = useRef(null);
+  const contactusRef = useRef(null);
+  function aboutusScroll() {
+    aboutusRef.current.scrollIntoView();
   }
-  function contactusScroll(){
-    contactusRef.current.scrollIntoView()
+  function contactusScroll() {
+    contactusRef.current.scrollIntoView();
   }
 
   const settings = {
@@ -80,36 +79,56 @@ function Landing() {
     ],
   };
 
-
-  const {width} = useWindowDimensions()
-  let programCount = 3
-  if(width<500) programCount = 1
-  else if(width>=500 && width<800) programCount=2
+  const { width } = useWindowDimensions();
+  let programCount = 3;
+  if (width < 500) programCount = 1;
+  else if (width >= 500 && width < 800) programCount = 2;
   //console.log(width, height)
   return (
     <>
-      <div className='landing'>
-        <img src={bgImage3} className='bgImage firstBgImage' alt="bgImage"/>
-        <Navbar aboutusScroll={aboutusScroll} contactusScroll={contactusScroll}/>
+      <div className="landing">
+        {/* <img src={bgImage3} className="bgImage firstBgImage" alt="bgImage" /> */}
+        <Navbar
+          aboutusScroll={aboutusScroll}
+          contactusScroll={contactusScroll}
+        />
         <div className="landing-section1">
           <div></div>
           <div className="content">
-            <h1>Restoring <img src={fire} alt="Fire Icon"/><br />your movement with physiotherapy</h1>
-            <div className='subContent'>
-            <p><b>Develop, Maintain and Restore your body’s movement and function with the world’s best physiotherapist</b></p>
+            <h1>
+              Restoring <img src={fire} alt="Fire Icon" />
+              <br />
+              your movement with physiotherapy
+            </h1>
+            <div className="subContent">
+              <p>
+                <b>
+                  Develop, Maintain and Restore your body’s movement and
+                  function with the world’s best physiotherapist
+                </b>
+              </p>
             </div>
-            <div className='section1-buttons'>
-              <button className='startNowButton' onClick={()=>{navigate('/dashboard')}}><b>Start now</b></button>
-              <div className='howItWorks'>
-                <div className='playButtonCircle'>
+            <div className="section1-buttons">
+              <button
+                className="startNowButton"
+                onClick={() => {
+                  navigate("/dashboard");
+                }}
+              >
+                <b>Start now</b>
+              </button>
+              <div className="howItWorks">
+                <div className="playButtonCircle">
                   <div className="playButtonTriangle"></div>
                 </div>
-              <p><b>How it works</b></p>
+                <p>
+                  <b>How it works</b>
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className='landing-trendingPrograms'>
+        {/* <div className='landing-trendingPrograms'>
           <img src={trophy} alt="Trophy Icon"/>
           <h1>Trending Programs</h1>
           <p>Try popular programs that suit yourself</p>
@@ -191,29 +210,59 @@ function Landing() {
           <p>See their testimonials</p>
           </div>
           <TestimonialCard />
-        </div>
+        </div> 
       </div>
-        <div className="landing-footer" ref={contactusRef}>
-          <div className='footer-content'>
-
+      <div className="landing-footer" ref={contactusRef}>
+        <div className="footer-content">
           <h1>Contact Us</h1>
           <p>
-            Heath Matthews Physio brings you better health and fitness through technology and expertise.
-            Everyone from Olympians to weekend warriors can benefit from Heath Matthews Physio to heal faster, perform better and feel their best.
+            Heath Matthews Physio brings you better health and fitness through
+            technology and expertise. Everyone from Olympians to weekend
+            warriors can benefit from Heath Matthews Physio to heal faster,
+            perform better and feel their best.
           </p>
-          <div className='footer-icons'><img src={emailImg} alt="email icon" style={{width:"1rem",height:"1rem"}}/><p>support@heathmatthewsphysio.com</p></div>
-          
-          <div className='footer-icons'><img src={phoneImg} alt="phone icon" style={{width:"1rem",height:"1rem"}}/><p>+91 8928444854</p></div>
+          <div className="footer-icons">
+            <img
+              src={emailImg}
+              alt="email icon"
+              style={{ width: "1rem", height: "1rem" }}
+            />
+            <p>support@heathmatthewsphysio.com</p>
           </div>
-        <form>      
-          <input name="name" type="text" className="feedback-input" placeholder="Name" />   
-          <input name="email" type="text" className="feedback-input" placeholder="Email" />
-          <textarea name="text" className="feedback-input" placeholder="Comment"></textarea>
-          <input type="submit" value="Submit"/>
-        </form>
+
+          <div className="footer-icons">
+            <img
+              src={phoneImg}
+              alt="phone icon"
+              style={{ width: "1rem", height: "1rem" }}
+            />
+            <p>+91 8928444854</p>
+          </div>
         </div>
+        <form>
+          <input
+            name="name"
+            type="text"
+            className="feedback-input"
+            placeholder="Name"
+          />
+          <input
+            name="email"
+            type="text"
+            className="feedback-input"
+            placeholder="Email"
+          />
+          <textarea
+            name="text"
+            className="feedback-input"
+            placeholder="Comment"
+          ></textarea>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>  */}
+      </div>
     </>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
